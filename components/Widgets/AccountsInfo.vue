551699722a6f24572b6ac62c71d8dd134d4b8b12
Ruslan <shadura.rus@gmail.com>
2025-03-29 14:06:07 +0200

    fix

--- components/Widgets/AccountsInfo.vue
+++ components/Widgets/AccountsInfo.vue
@@ -2,6 +2,13 @@
 import type { ECurrency } from '@/types/enums/currency'
 
 const { list: accountsList, isListLoading } = useAccounts()
+
+const splitedAccounts = computed(() => {
+	return {
+		netWorth: accountsList.value.filter((account) => account.is_net_worth),
+		current: accountsList.value.filter((account) => !account.is_net_worth),
+	}
+})
 </script>
 
 <template>
@@ -15,18 +22,45 @@ const { list: accountsList, isListLoading } = useAccounts()
 				<Loader />
 			</div>
 
-			<ul v-else class="mt-4">
-				<li v-for="account in accountsList" :key="account.id" class="flex gap-1 justify-between mb-2 pb-2 border-b">
-					<div>
-						<span class="inline-block mr-1">{{ account.name }}</span>
-						<Tag>{{ account.type }}</Tag>
-					</div>
+			<template v-else>
+				<Separator class="my-4" label="Current" />
 
-					<div class="text-right">
-						{{ useFormatAmount(account.balance || 0, account.currency as ECurrency) }}
-					</div>
-				</li>
-			</ul>
+				<ul class="mt-4">
+					<li
+						v-for="account in splitedAccounts.current"
+						:key="account.id"
+						class="flex gap-1 justify-between mb-2 pb-2 border-b"
+					>
+						<div>
+							<span class="inline-block mr-1">{{ account.name }}</span>
+							<Tag>{{ account.type }}</Tag>
+						</div>
+
+						<div class="text-right">
+							{{ useFormatAmount(account.balance || 0, account.currency as ECurrency) }}
+						</div>
+					</li>
+				</ul>
+
+				<Separator class="my-4" label="Net worth" />
+
+				<ul class="mt-4">
+					<li
+						v-for="account in splitedAccounts.netWorth"
+						:key="account.id"
+						class="flex gap-1 justify-between mb-2 pb-2 border-b"
+					>
+						<div>
+							<span class="inline-block mr-1">{{ account.name }}</span>
+							<Tag>{{ account.type }}</Tag>
+						</div>
+
+						<div class="text-right">
+							{{ useFormatAmount(account.balance || 0, account.currency as ECurrency) }}
+						</div>
+					</li>
+				</ul>
+			</template>
 		</CardContent>
 	</Card>
 </template>
