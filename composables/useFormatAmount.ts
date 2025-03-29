7c89ebcdae85bb6d8c8e2303cfc7542f215fee5e
Ruslan <shadura.rus@gmail.com>
2025-03-29 13:57:24 +0200

    fix

--- composables/useFormatAmount.ts
+++ composables/useFormatAmount.ts
@@ -1,9 +1,7 @@
 import type { ECurrency } from '~/types/enums/currency'
 
 const useFormatAmount = (amount: number, currency?: ECurrency) => {
-	if (!amount) return ''
-
-	const formattedNumber = amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+	const formattedNumber = (amount || 0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
 	return currency ? `${formattedNumber} ${currency || ''}` : formattedNumber
 }
 
