0719441748463c6fba59ee51fb7ece8c2a7d606c
Ruslan <shadura.rus@gmail.com>
2025-03-29 14:30:58 +0200

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 		<AppSidebar />
 		<main class="w-full">
 			<SidebarTrigger />
-			<div class="pt-6 sm:px-6 lg:px-8 w-full"><slot /></div>
+			<div class="pt-6 sm:px-6 lg:px-8 w-full pb-6"><slot /></div>
 		</main>
 	</SidebarProvider>
 </template>
